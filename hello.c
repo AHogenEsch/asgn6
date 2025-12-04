1641fced1b1b9b6cd528d16a4637ef9af444046f
arh <ahogenes@calpoly.edu>
2025-12-04 13:04:53 -0800

    comma

--- hello.c
+++ hello.c
@@ -3,7 +3,7 @@
 /* Prints "Hello world!" or exits with status 1 if failure*/
 int main(int argc, char *argv[]){
     /* Greet the world, but don't forget about errors */
-    if(printf("Hello world!\n") < 0){
+    if(printf("Hello, world!\n") < 0){
         fprintf(stderr, "Error: printf failed to write the message.\n");
         return 1;
     }
