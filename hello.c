c7eb0f934102d6499e04f93d50decbf2601a14eb
arh <ahogenes@calpoly.edu>
2025-12-04 13:03:46 -0800

    added robust error checking

--- hello.c
+++ hello.c
@@ -1,9 +1,13 @@
 #include "hello.h"
 
+/* Prints "Hello world!" or exits with status 1 if failure*/
 int main(int argc, char *argv[]){
-    // Greet the world
-    printf("Hello world!\n");
-    // Exit cleanly with no errors
+    /* Greet the world, but don't forget about errors */
+    if(printf("Hello world!\n") < 0){
+        fprintf(stderr, "Error: printf failed to write the message.\n");
+        return 1;
+    }
+    /* Exit cleanly if no errors */
     return 0;
-    // Have a good winter break!
+    /* Have a good winter break! */
 }
--- hello.h
+++ hello.h
@@ -1,4 +1,4 @@
-// Required library
+/* Required library */
 #include <stdio.h>
-// Function prototype
+/* Function prototype */
 int main(int argc, char *argv[]);
